5ac8231a605f787b11b75e3ba9789f42c8d885af
Thibault <thibault.rieul@gmail.com>
2020-03-22 00:16:31 +0100

    menu 1 a 4

--- Test.py
+++ Test.py
@@ -5,51 +5,109 @@ from bs4 import BeautifulSoup
 import requests
 import json
 import pandas as pd
+import time
+import os
 
-i = 1
-j = 1
-y = 2
-m = ""
-jo = ""
-d = ""
-Ldate = []
-while y < 7:
-	while j < 13:
-		while i < 32:
-			if j < 10:
-                m = "0"+str(j)
-            else:
-                m=str(j)
-            if i<10:
-                jo="0"+str(i)
-            else:
-                jo=str(i)
-            d=m+"/"+jo+"/201"+y
-            print(d)
-			# commande d'insertion dans la base
-			i+=1
-		j+=1
-        i=1	
-    y+=1
-    j=1
+client = MongoClient()
+db = client.Morts #Creation database
+Victimes = db.Victimes #Creation Collection
 
+Liste_drogues=['Heroin','Cocaine','Fentanyl','FentanylAnalogue','Oxycodone','Oxymorphone','Ethanol','Hydrocodone','Benzodiazepine','Methadone','Amphet','Tramad','Morphine_NotHeroin','Hydromorphone','Other','OpiateNOS','AnyOpioid']
+ListeVille=["HARTFORD","NEW HAVEN","WATERBURY","BRIDGEPORT", "NEW BRITAIN", "MERIDEN","NORWICH","BRISTOL","NEW LONDON","DANBURY", "TORRINGTON","MANCHESTER","MIDDLETOWN","ENFIELD","STAMFORD","EAST HARTFORD", "MILFORD", "WEST HAVEN","NORWALK","DERBY"]
+Arret_prog=False
 
-
-dt=pd.read_csv("test.csv",sep=',')
-dt.sort_values(by='ID')
-print(dt)
-
-
-# with open('data.json') as f:
-    # file_data = json.load(f)
-
-# collection_currency.insert_one(file_data)
-
-
-# client = MongoClient()
-# db = client.test_database #Creation database
-# testcollec = db.testcollec #Creation Collection
-# doc_id = testcollec.insert_one({"name":'bureau', "size":12}).inserted_id #insertion document dans collection
-# print(doc_id)
-# print(db.list_collection_names())
-# print(testcollec.find_one({"name": "bureau"})) #recherche doc par un critere
+choix_m=0
+choix_drug=""
+while not Arret_prog:
+	os.system('cls' if os.name == 'nt' else 'clear')
+	print("Bienvenue sur le projet de Valentin BOUDON et Thibault RIEUL \n\n")
+	print("Veuillez choisir une des requetes proposées:\n")
+	print("Choix 1: Nombre de mort pour une drogue choisie et un age choisi ")
+	print("Choix 2: Nombre de mort pour une drogue choisie et avant un age choisi ")
+	print("Choix 3: Nombre de mort pour une drogue choisie et après un age choisi ")
+	print("Choix 4: Nombre de mort pour une drogue choisie et un sexe choisi  ")
+	print("Choix 5: Nombre de mort pour une drogue choisie et une ethnie choisie")
+	print("Choix 6: Nombre de mort pour une ville choisie et un age choisi ")
+	print("Choix 7: Nombre de mort pour une ville choisie et un sexe choisi ")
+	print("Choix 8: Nombre de mort pour une ville choisie et après un age choisi ")
+	print("Choix 9: Nombre de mort pour une ville choisie et avant un age choisi ")
+	print("Choix 10: Nombre de mort pour une ville choisie et à un age choisi ")
+	print("Choix 10: Nombre de mort pour une ethnie choisie et à un sexe choisi ")
+	print("Choix 11: La ville ayant eu le plus de mort ")
+	print("Choix 12: La drogue etant presente chez le plus de morts")
+	print("Choix 13: La drogue etant presente chez le plus de morts après un age choisi ")
+	print("Choix 14: La drogue etant presente chez le plus de morts avant un age choisi ")
+	print("Choix 15: La moyenne d'age des victimes ")
+	print("Choix 16: Quitter le programme ")
+	print("Tapez le numero de la requete désirée")
+	reponse=int(input())
+	while reponse>17 & reponse<0:
+		print("Ce numero ne correspond a aucun choix, veuillez le modifier: \n Entrez votre choix:\n")
+		reponse=int(input())
+	os.system('cls' if os.name == 'nt' else 'clear')
+	if reponse==1:
+		print("Veuillez choisir une drogue parmis cette liste: \n Heroin, Cocaine, Fentanyl, FentanylAnalogue, Oxycodone, Oxymorphone, Ethanol, Hydrocodone, Benzodiazepine, Methadone, Amphet, Tramad, Morphine_NotHeroin, Hydromorphone, Other, OpiateNOS, AnyOpioid\n et entrer son nom:")
+		choix_drug=str(input())
+		while choix_drug not in Liste_drogues:
+			print("Cette drogue ne correspond a aucun choix, veuillez modifier votre choix: \n Entrez votre choix:\n")
+			choix_drug=str(input())
+		print("Veuillez choisir un age:")
+		choix=int(input())
+		temp=Victimes.find({choix_drug:"Y"})
+		count=0
+		for x in temp:
+			if int(x["Age"])==choix:
+				count+=1
+		print("Il y a "+str(count)+" victimes correspondant à votre demande")
+		Arret_prog=True
+	if reponse==2:
+		print("Veuillez choisir une drogue parmis cette liste: \n Heroin, Cocaine, Fentanyl, FentanylAnalogue, Oxycodone, Oxymorphone, Ethanol, Hydrocodone, Benzodiazepine, Methadone, Amphet, Tramad, Morphine_NotHeroin, Hydromorphone, Other, OpiateNOS, AnyOpioid\n et entrer son nom:")
+		choix_drug=str(input())
+		while choix_drug not in Liste_drogues:
+			print("Cette drogue ne correspond a aucun choix, veuillez modifier votre choix: \n Entrez votre choix:\n")
+			choix_drug=str(input())
+		print("Veuillez choisir un age:")
+		choix=int(input())
+		temp=Victimes.find({choix_drug:"Y"})
+		count=0
+		for x in temp:
+			if int(x["Age"])<choix:
+				count+=1
+		print("Il y a "+str(count)+" victimes correspondant à votre demande")
+		Arret_prog=True
+	if reponse==3:
+		print("Veuillez choisir une drogue parmis cette liste: \n Heroin, Cocaine, Fentanyl, FentanylAnalogue, Oxycodone, Oxymorphone, Ethanol, Hydrocodone, Benzodiazepine, Methadone, Amphet, Tramad, Morphine_NotHeroin, Hydromorphone, Other, OpiateNOS, AnyOpioid\n et entrer son nom:")
+		choix_drug=str(input())
+		while choix_drug not in Liste_drogues:
+			print("Cette drogue ne correspond a aucun choix, veuillez modifier votre choix: \n Entrez votre choix:\n")
+			choix_drug=str(input())
+		print("Veuillez choisir un age:")
+		choix=int(input())
+		temp=Victimes.find({choix_drug:"Y"})
+		count=0
+		for x in temp:
+			if int(x["Age"])>choix:
+				count+=1
+		print("Il y a "+str(count)+" victimes correspondant à votre demande")
+		Arret_prog=True
+	if reponse==4:
+		print("Veuillez choisir une drogue parmis cette liste: \n Heroin, Cocaine, Fentanyl, FentanylAnalogue, Oxycodone, Oxymorphone, Ethanol, Hydrocodone, Benzodiazepine, Methadone, Amphet, Tramad, Morphine_NotHeroin, Hydromorphone, Other, OpiateNOS, AnyOpioid\n et entrer son nom:")
+		choix_drug=str(input())
+		while choix_drug not in Liste_drogues:
+			print("Cette drogue ne correspond a aucun choix, veuillez modifier votre choix: \n Entrez votre choix:\n")
+			choix_drug=str(input())
+		print("\nVeuillez choisir un sexe: Male ou Female")
+		choix=str(input())
+		while choix not in ["Male","Female"]:
+			print("Ce sexe ne correspond a aucun choix, veuillez modifier votre choix: \n Entrez votre choix:\n")
+			choix_drug=str(input())
+		temp=Victimes.find({choix_drug:"Y"})
+		count=0
+		for x in temp:
+			if str(x["Sex"])==choix:
+				count+=1
+		print("\nIl y a "+str(count)+" victimes correspondant à votre demande")
+		Arret_prog=True
+	if reponse==16:
+			Arret_prog=True
+				
--- projet.py
+++ projet.py
@@ -5,20 +5,21 @@ from bs4 import BeautifulSoup
 import requests
 import json
 import pandas as pd
+import time
+import os
+
 
 dt=pd.read_csv("test.csv",sep=',')
 tridate=dt.sort_values('ID')
 
+
+
 client = MongoClient()
-db = client.Mort #Creation database
+db = client.Morts #Creation database
 Victimes = db.Victimes #Creation Collection
-for i in range(5100):
-    
-	Victime_id = Victimes.insert_one({"Age":tridate.iloc[i]['Age'], "size":12}).inserted_id #insertion document dans collection
-
-
-
-
+for i in range(30):  
+	Victime_id = Victimes.insert_one({"Date":tridate.iloc[i]['Date'],"Age":tridate.iloc[i]['Age'], "Sex":tridate.iloc[i]['Sex'],"Race":tridate.iloc[i]['Race'],"DeathCity":tridate.iloc[i]['DeathCity'],"Heroin":tridate.iloc[i]['Heroin'],'Cocaine':tridate.iloc[i]['Cocaine'],'Fentanyl':tridate.iloc[i]['Fentanyl'],'FentanylAnalogue':tridate.iloc[i]['FentanylAnalogue'],'Oxycodone':tridate.iloc[i]['Oxycodone'],'Oxymorphone':tridate.iloc[i]['Oxymorphone'],'Ethanol':tridate.iloc[i]['Ethanol'],'Hydrocodone':tridate.iloc[i]['Hydrocodone'],'Benzodiazepine':tridate.iloc[i]['Benzodiazepine'],'Methadone':tridate.iloc[i]['Methadone'],'Amphet':tridate.iloc[i]['Amphet'],'Tramad':tridate.iloc[i]['Tramad'],'Morphine_NotHeroin':tridate.iloc[i]['Morphine_NotHeroin'],'Hydromorphone':tridate.iloc[i]['Hydromorphone'],'Other':tridate.iloc[i]['Other'],'OpiateNOS':tridate.iloc[i]['OpiateNOS'],'AnyOpioid':tridate.iloc[i]['AnyOpioid']}).inserted_id #insertion document dans collection
+	#time.sleep(.1)
 
 
 
