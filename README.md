e5b3092e32250b2a5c7f9d5766bc705137cb72b8
Thibault <thibault.rieul@gmail.com>
2020-03-21 15:59:27 +0100

    lecture data

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # Projet-NoSQL
 Projet NoSQL de Valentin BOUDON et Thibault RIEUL
+url = "https://data.ct.gov/api/views/rybz-nyjw/rows.json"
+
+headers = {
+    'x-rapidapi-host': "montanaflynn-fifa-world-cup.p.rapidapi.com",
+    'x-rapidapi-key': "948ca828c0mshe8fd4cb616ef4afp1372fbjsnd5c9fcc4115c",
+    'accepts': "json"
+    }
+
+response = requests.request("GET", url, headers=headers)
+Data=response.json
+
+print(response.text)
--- Test.py
+++ Test.py
@@ -1,10 +1,30 @@
 import pymongo
 from pymongo import MongoClient
-
+from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup
+import requests
+import json
+import pandas as pd
 client = MongoClient()
-db = client.test_database
-testcollec = db.testcollec
-doc_id = testcollec.insert_one({"name":'bureau', "size":12}).inserted_id
-print(doc_id)
-print(db.list_collection_names())
-print(posts.find_one({"name": "bureau"}))
+db = client.mortdrogue
+collection_currency = db.victimes
+
+data = json.load(open('data.json'))
+
+df = pd.DataFrame(data["data"])
+
+print(df)
+
+#with open('data.json') as f:
+    #file_data = json.load(f)
+
+#collection_currency.insert_one(file_data)
+
+
+#client = MongoClient()
+#db = client.test_database #Creation database
+#testcollec = db.testcollec #Creation Collection
+#doc_id = testcollec.insert_one({"name":'bureau', "size":12}).inserted_id #insertion document dans collection
+#print(doc_id)
+#print(db.list_collection_names())
+#print(testcollec.find_one({"name": "bureau"})) #recherche doc par un critere
