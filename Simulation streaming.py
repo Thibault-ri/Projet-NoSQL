da3dba9a14e1110c678be0a62d401868cbef4b31
Thibault <thibault.rieul@gmail.com>
2020-03-22 02:22:02 +0100

    SImulation streaming

--- Simulation streaming.py
+++ Simulation streaming.py
@@ -21,7 +21,7 @@ db = client.Morts #Creation database
 Victimes = db.Victimes #Creation Collection
 for i in range(5000):  
 	Victime_id = Victimes.insert_one({"Date":tridate.iloc[i]['Date'],"Age":tridate.iloc[i]['Age'], "Sex":tridate.iloc[i]['Sex'],"Race":tridate.iloc[i]['Race'],"DeathCity":tridate.iloc[i]['DeathCity'],"Heroin":tridate.iloc[i]['Heroin'],'Cocaine':tridate.iloc[i]['Cocaine'],'Fentanyl':tridate.iloc[i]['Fentanyl'],'FentanylAnalogue':tridate.iloc[i]['FentanylAnalogue'],'Oxycodone':tridate.iloc[i]['Oxycodone'],'Oxymorphone':tridate.iloc[i]['Oxymorphone'],'Ethanol':tridate.iloc[i]['Ethanol'],'Hydrocodone':tridate.iloc[i]['Hydrocodone'],'Benzodiazepine':tridate.iloc[i]['Benzodiazepine'],'Methadone':tridate.iloc[i]['Methadone'],'Amphet':tridate.iloc[i]['Amphet'],'Tramad':tridate.iloc[i]['Tramad'],'Morphine_NotHeroin':tridate.iloc[i]['Morphine_NotHeroin'],'Hydromorphone':tridate.iloc[i]['Hydromorphone'],'Other':tridate.iloc[i]['Other'],'OpiateNOS':tridate.iloc[i]['OpiateNOS'],'AnyOpioid':tridate.iloc[i]['AnyOpioid']}).inserted_id #insertion document dans collection
-	#time.sleep(.1)
+	time.sleep(.1)
 
 
 
